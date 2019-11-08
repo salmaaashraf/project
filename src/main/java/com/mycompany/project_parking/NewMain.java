6771072dbf0d0151563fe1876fcf484c0cc84710
user20002 <salmaashraf20002000@gmail.com>
2019-11-08 10:42:02 +0200

    initial commit

---
+++ src/main/java/com/mycompany/project_parking/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project_parking;
+
+/**
+ *
+ * @author metr
+ */
+import java.util.*;
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("welcome to our project\n");
+    }
+    
+}
